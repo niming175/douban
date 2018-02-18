7e1ec90c22781ac289d14347012c54d96d018c1d
root <root@DESKTOP-72PMRLN.localdomain>
2018-02-18 23:30:27 +0800

    添加了状态页面（status.vue）

---
+++ src/components/userBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="user-bar">
+    <div class="user-bar__img">
+      <img src="" alt="">
+    </div>
+    <div class="user-bar__name">{{holder}}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'userBar',
+  computed: {
+    holder () {
+      return this.currentUser.email ? this.currentUser.email : '请登陆'
+    },
+    ...mapGetters(['currentUser'])
+  },
+  created () {
+    if (localStorage.getItem('email')) {
+      this.$store.commit({
+        type: 'getUserInfo'
+      })
+    }
+  }
+}
+</script>
--- src/pages/Status.vue
+++ src/pages/Status.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="status">
     <banner class="status__banner"></banner>
+    <user-bar></user-bar>
   </div>
 </template>
 <script>
 import banner from '../components/Banner01'
+import userBar from '../components/userBar'
 export default {
   name: 'status',
   components: {
-    banner
+    banner,
+    userBar
   }
 }
 </script>
